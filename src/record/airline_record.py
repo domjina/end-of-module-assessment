f29b180ad35ef7f0f93a4e403771e7c0d03f8762
Dom Jina <dominicjina@hotmail.com>
2026-09-09 14:30:18 +0100

    Record: Enforce valid record types.
    Ensure ClientRecord instances can only use the "client" record type and AirlineRecord instances can only use the "airline" record type.
    
    Validate the record type when constructing each dataclass. This prevents created and updated records having invalid values for record type values.

--- src/record/airline_record.py
+++ src/record/airline_record.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
+from src.record.record_types import RecordType
 
 @dataclass(frozen=True)
 class AirlineRecord:
     id: int
     record_type: str
     company_name: str
+
+    def __post_init__(self):
+        if self.record_type != RecordType.AIRLINE.value:
+            raise ValueError("Airline record_type must be 'airline'")
